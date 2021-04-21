e1594bc62d3e94915154bb761cd08e3394b12068
anatarantino <58373829+anatarantino@users.noreply.github.com>
2021-04-20 22:01:47 -0300

    cambio definitivo de mensaje de error :)

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -6,7 +6,7 @@
           <v-img src="../assets/images/logo.png"
                  :max-height="this.$vuetify.breakpoint.smAndDown ? '120':'180'" contain/>
           <h1 class="text-center text-h5 text-md-h3 font-weight-bold mb-5">Error 404</h1>
-          <h2 class="text-center text-h6 text-md-h4 font-weight-medium my-5">Invalid url path.</h2>
+          <h2 class="text-center text-h6 text-md-h4 font-weight-medium my-5">Ruta de acceso de dirección url no válida.</h2>
         </v-card>
       </v-col>
     </v-row>
