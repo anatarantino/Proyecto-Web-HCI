f51c591cb6e030feba4ee93b7fd87649f8c8962a
anatarantino <58373829+anatarantino@users.noreply.github.com>
2021-05-02 23:46:50 -0300

    agregado imports store

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import user from './modules/user/index'
+import api from './modules/api/index'
+import routines from './modules/routines/index'
 
 Vue.use(Vuex);
 
 
 const store={
   modules:{
-    api,routines,user,
+    api,routines,user
   }
 }
 
 export default new Vuex.Store({
-
+  ...store
 })
